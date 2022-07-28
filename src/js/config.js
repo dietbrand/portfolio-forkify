acc6df38c27e2b2f997637eff5f427153e4aa729
Dietbrand <dietbrand.tollenaere@gmail.com>
2022-07-28 14:47:31 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_KEY = '173c134b-0576-4477-b04f-9fd34e121376';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
